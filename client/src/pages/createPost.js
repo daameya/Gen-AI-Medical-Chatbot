4065f12f53e289cfcb8c6e98aed2a38157f0844c
daameya <1998ameya@gmail.com>
2024-08-01 16:00:00 +0530

    createPost.js updated

--- client/src/pages/createPost.js
+++ client/src/pages/createPost.js
@@ -2,22 +2,7 @@ import { useState } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 import { Navigate } from "react-router-dom";
-
-const modules = {
-    toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
-      ['clean']
-    ],
-  };
-  const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
-  ];
+import Editor from "../editor";
 
 export default function CreatePost() {
     const [title,setTitle] = useState('');
@@ -37,7 +22,7 @@ export default function CreatePost() {
             body: data,
             credentials: 'include',
         });
-        if (response.ok){
+        if (response.ok) {
             setRedirect(true);
         }
     }
@@ -45,7 +30,6 @@ export default function CreatePost() {
     if (redirect) {
         return <Navigate to={'/'} />
     }
-
     return (
         <form onSubmit={createNewPost}>
             <input type="title" 
@@ -58,10 +42,7 @@ export default function CreatePost() {
                 onChange={ev => setSummary(ev.target.value)} />
             <input type="file"
                 onChange={ev => setFiles(ev.target.files)} />
-            <ReactQuill value={content} 
-                onChange={newValue => setContent(newValue)}
-                module={modules} 
-                formats={formats} />
+            <Editor value={content} onChange={setContent} />
             <button style={{marginTop:'5px'}}>Publish</button>
         </form>
     );
