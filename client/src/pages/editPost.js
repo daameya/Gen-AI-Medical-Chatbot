937ad8bf94d6587ee0d69b2889b726245ec28776
daameya <1998ameya@gmail.com>
2024-08-04 13:00:00 +0530

    postPage.js updated

--- client/src/pages/editPost.js
+++ client/src/pages/editPost.js
@@ -11,15 +11,14 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-        fetch('http://localhost:4000/post/'+id)
-            .then(response => {
-                    response.json().then(postInfo => {
-                        setTitle(postInfo.title);
-                        setContent(postInfo.content);
-                        setSummary(postInfo.summary);
-                    });
-            });
-    }, []);
+    fetch('http://localhost:4000/post/'+id).then(response => {
+      response.json().then(postInfo => {
+        setTitle(postInfo.title);
+        setContent(postInfo.content);
+        setSummary(postInfo.summary);
+      });
+    });
+  }, []);
 
   async function updatePost(ev) {
     ev.preventDefault();
