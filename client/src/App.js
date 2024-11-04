b4974ea04a539790fd0df253e639f8b523ff625d
daameya <1998ameya@gmail.com>
2024-11-04 23:27:15 +0530

    Authentication and logout page created

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,20 @@ import Layout from './layout';
 import HomePage from './pages/homePage';
 import LoginPage from './pages/loginPage';
 import RegisterPage from './pages/registerPage';
+import { UserContextProvider } from './UserContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-      
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+    
   );
 }
 
