6e1531db047870d8a4d390d72c1e0716feb392a8
daameya <1998ameya@gmail.com>
2024-12-02 14:00:00 +0530

    README.md updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen AI Medical Chatbot',
+    version= '0.0.0',
+    author= 'Ameya Damle',
+    author_email= '1998ameya@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
